f558b6a551ac80e6b5adc1b810396375755f2d33
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-05-30 11:44:49 +0300

    Crypto

--- api.py
+++ api.py
@@ -3,12 +3,28 @@ import os
 import requests
 dotenv.load_dotenv()
 
+
 class API:
     def __init__(self):
-        dotenv.load_dotenv()
         self.key = os.getenv('API_KEY')
+        self.base_url = 'https://rest.coinapi.io/v1/'
+
+    def make_request(self, endpoint, params=None):
+        url = f"{self.base_url}{endpoint}"
+        headers = {
+            'X-CoinAPI-Key': self.key
+        }
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        return response.json()
 
 
 class MarketEndpoint(API):
-    pass
+    def get_exchange_rates(self, asset_id_base, asset_id_quote):
+        endpoint = f"exchangerate/{asset_id_base}/{asset_id_quote}"
+        return self.make_request(endpoint)
+
+    def list_assets(self):
+        endpoint = "assets"
+        return self.make_request(endpoint)
 
